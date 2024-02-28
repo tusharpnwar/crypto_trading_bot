79b9548a37c840c4bc41c9a76d10236130dfa8dd
rounakpaul11 <142620722+rounakpaul11@users.noreply.github.com>
2024-02-28 14:14:31 +0530

    DESIGNING

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import base64
 import pandas as pd
 import yfinance as yf
 import joblib
@@ -8,6 +9,43 @@ import plotly.graph_objects as go
 
 # Load the ARIMA model
 arima_model = joblib.load('arima_model.joblib')
+
+@st.experimental_memo
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("IMAGES.jpg")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+    background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+    background-size: 180%;
+    background-position: top left;
+    background-repeat: no-repeat;
+    background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+    background-image: url("data:image/png;base64,{img}");
+    background-position: center; 
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+    right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Define functions for data fetching and prediction
 def fetch_data(ticker, start_date, end_date):
