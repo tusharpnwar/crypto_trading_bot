8dc775375691c9651b0416fd225dc6473ad77dd2
rounakpaul11 <142620722+rounakpaul11@users.noreply.github.com>
2024-02-28 14:31:03 +0530

    DESIGNING

--- bot.py
+++ bot.py
@@ -21,14 +21,19 @@ def get_img_as_base64(file):
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-    background-image: url("IMAGES.JPG");
+    background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
     background-size: 180%;
     background-position: top left;
     background-repeat: no-repeat;
     background-attachment: local;
 }}
 
-
+[data-testid="stSidebar"] > div:first-child {{
+    background-image: url("data:image/png;base64,{img}");
+    background-position: center; 
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}}
 
 [data-testid="stHeader"] {{
     background: rgba(0,0,0,0);
