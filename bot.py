b25cd73e2e56f6d07b5044e929bd18805d540fb1
ROUNAK PAUL <paulrounak999@gmail.com>
2024-03-21 15:34:31 +0530

    sliders

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ import numpy as np
 import streamlit as st
 import plotly.graph_objects as go
 from openai import OpenAI
+import random
+import time
 
 # Load the ARIMA model
 arima_model = joblib.load('arima_model.joblib')
@@ -154,32 +156,42 @@ if __name__ == '__main__':
 # Echo Bot integration as a sidebar
 st.sidebar.title("ChatGPT-like clone")
 
-client = OpenAI(api_key="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")
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+st.title("Simple chat")
+
+# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+# Accept user input
 if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
